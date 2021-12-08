737cc1f9ae5f96681008bea4de9eabe7a3caed8e
Dior <iamdiar31@gmail.com>
2021-12-08 12:54:41 +0500

    Pow; Sqrt; Abs; Round; Ceiling; Floor; Min; Max

---
+++ 10) math_class.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Math class
+
+            double x = 3.49;
+
+            // double a = Math.Pow(x, 3);                       EX: 3 ^ 3 = 27
+            // double b = Math.Sqrt(x);                          EX: √3 = 1.73
+            // double c = Math.Abs(x);                    EX: -3 = 3; -32 = 32
+            // double d = Math.Round(x);                EX: 3.49 = 3; 3.51 = 4
+            // double e = Math.Ceiling(x);            EX: 3.49 = 4; -3.49 = -3
+            // double f = Math.Floor(x);              EX: 3.49 = 3; -3.49 = -4
+
+            double x = 3.99;
+            double y = 4;
+
+            // double g = Math.Max(x, y);         EX: x = 3.99, y = 4, max = 4
+            // double h = Math.Min(x, y);      EX: x = 3.99, y = 4, min = 3.99
+
+            System.Console.WriteLine(h);
+
+            // wait before closing
+            Console.ReadKey();
+        }
+    }
+}
